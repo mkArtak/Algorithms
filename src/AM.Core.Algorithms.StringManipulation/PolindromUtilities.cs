31bbba47393e952891b8968983052bd746f1486e
Artak Mkrtchyan <artak@outlook.com>
2017-06-13 22:53:19 -0700

    Added PolindromeUtilities and Tests

---
+++ src/AM.Core.Algorithms.StringManipulation/PolindromUtilities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AM.Core.Algorithms.StringManipulation
+{
+    public static class PolindromUtilities
+    {
+        public static bool IsPolindrome(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            if (input.Length > 1)
+            {
+                int currentIndex = 0;
+                int mirrorIndex = input.Length - 1;
+
+                do
+                {
+                    if (input[currentIndex] != input[mirrorIndex])
+                    {
+                        return false;
+                    }
+
+                } while (++currentIndex < --mirrorIndex);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ test/AM.Core.Algorithms.StringManipulation.Tests/PolindromUtilitiesTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace AM.Core.Algorithms.StringManipulation.Tests
+{
+    public class PolindromUtilitiesTests
+    {
+        [Fact]
+        public void IsPlindrome_FailsForNullArgument()
+        {
+            Assert.Throws<ArgumentNullException>(() => PolindromUtilities.IsPolindrome(null));
+        }
+
+        [Fact]
+        public void IsPolindrome_ReturnsTrueForEmptyString()
+        {
+            Assert.True(PolindromUtilities.IsPolindrome(string.Empty));
+        }
+
+        [Fact]
+        public void IsPolindrome_ReturnsFalseForNonPolindromeString()
+        {
+            Assert.False(PolindromUtilities.IsPolindrome("abcd"));
+        }
+
+        [Fact]
+        public void IsPolindrome_ReturnsTrueForPolindromeString()
+        {
+            Assert.True(PolindromUtilities.IsPolindrome("abba"));
+        }
+    }
+}
