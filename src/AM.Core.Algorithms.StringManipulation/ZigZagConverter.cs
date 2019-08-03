5ab74c8ae4c2cb11f7ed1c86b1b39fc42cb35f3d
Artak Mkrtchyan <artak@outlook.com>
2019-08-03 10:51:21 -0700

    Implemented ZigZag convertion algorithm (non-optimal option)

---
+++ src/AM.Core.Algorithms.StringManipulation/ZigZagConverter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+
+namespace AM.Core.Algorithms.StringManipulation
+{
+    public static class ZigZagConverter
+    {
+        // This is a brute-force implementation for zig-zag convertion problem. Not optimal at all.
+        public static string Convert(string s, int numRows)
+        {
+            if (numRows <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (s == null)
+                throw new ArgumentNullException();
+
+            if (s.Length <= 1 || numRows == 1)
+            {
+                return s;
+            }
+
+            if (s.Length <= numRows)
+            {
+                return s;
+            }
+
+            if (numRows == 2)
+            {
+                char[] sb = new char[s.Length];
+                int currentIndex = 0;
+                for (int i = 0; i < s.Length; i += 2)
+                {
+                    sb[currentIndex++] = s[i];
+                }
+                for (int i = 1; i < s.Length; i += 2)
+                {
+                    sb[currentIndex++] = s[i];
+                }
+
+                return string.Join(string.Empty, sb);
+            }
+            else
+            {
+                int rowDirection = 1;
+                int colDirection = 0;
+
+                int currentRow = 0;
+                int currentCol = 0;
+
+                char?[,] map = new char?[numRows, (1 + (s.Length / (2 * numRows - 2))) * (numRows - 1)];
+                for (int i = 0; i < s.Length; i++)
+                {
+                    map[currentRow, currentCol] = s[i];
+                    currentRow += rowDirection;
+                    currentCol += colDirection;
+
+                    if (currentRow == 0)
+                    {
+                        // At the top of the row. Start moving down veritically
+                        rowDirection = 1;
+                        colDirection = 0;
+                    }
+                    else if (currentRow == numRows - 1)
+                    {
+                        // At the bottom of the row. Start moving up diagonally
+                        rowDirection = -1;
+                        colDirection = 1;
+                    }
+                }
+
+                StringBuilder result = new StringBuilder(s.Length);
+                for (int i = 0; i < numRows; i++)
+                {
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+                        if (map[i, j] != null)
+                        {
+                            result.Append(map[i, j]);
+                        }
+                    }
+                }
+
+                return result.ToString();
+            }
+        }
+    }
+}
---
+++ test/AM.Core.Algorithms.StringManipulation.Tests/ZigZagConverterTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace AM.Core.Algorithms.StringManipulation.Tests
+{
+    public class ZigZagConverterTests
+    {
+        [Fact]
+        public void Convert_SucceedsForTwoRows()
+        {
+            const string input = "abcdefg";
+
+            Assert.Equal("acegbdf", ZigZagConverter.Convert(input, 2));
+        }
+
+        [Theory]
+        [InlineData("abc", 1, "abc")]
+        [InlineData("abcde", 3, "aebdc")]
+        [InlineData("ab", 2, "ab")]
+        [InlineData("abc", 2, "acb")]
+        [InlineData("abcde", "2", "acebd")]
+        [InlineData("abcdef", 3, "aebdfc")]
+        [InlineData("PAYPALISHIRING", 5, "PHASIYIRPLIGAN")]
+        public void Convert_Succeeds(string input, int numberOfRows, string expectedResult)
+        {
+            Assert.Equal(expectedResult, ZigZagConverter.Convert(input, numberOfRows));
+        }
+    }
+}
