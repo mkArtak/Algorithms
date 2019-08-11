f1f53787d1e813472d0b5a25eeb05fd617ef1155
Artak Mkrtchyan <artak@outlook.com>
2019-08-11 02:02:03 -0700

    Added an implementation of ToInt algorithm

---
+++ src/AM.Core.Algorithms.Numbers/NumberUtilities.cs
@@ -0,0 +1,82 @@
+﻿namespace AM.Core.Algorithms.Numbers
+{
+    public static class NumberUtilities
+    {
+        /// <summary>
+        /// Reverses the digits of the given integer.
+        /// </summary>
+        /// <param name="input">The number to reverse</param>
+        /// <returns>A new nuber with reversed digits. If the reversing process results in an overflow, the result will be 0.</returns>
+        public static int Reverse(int input)
+        {
+            int reversed = 0;
+            while (input != 0)
+            {
+                int digit = input % 10;
+                try
+                {
+                    checked
+                    {
+                        reversed = reversed * 10 + digit;
+                    }
+                }
+                catch
+                {
+                    return 0;
+                }
+
+                input /= 10;
+            }
+
+            return reversed;
+        }
+
+        public static int ToInt(string input)
+        {
+            if (input == null || input.Length == 0)
+            {
+                return 0;
+            }
+
+            int startIndex = 0;
+            while (input[startIndex++] == ' ') ;
+            startIndex--;
+
+            int sign = 1;
+            if (input[startIndex] == '+')
+            {
+                startIndex++;
+            }
+            else if (input[startIndex] == '-')
+            {
+                sign = -1;
+                startIndex++;
+            }
+
+            if (startIndex == input.Length)
+            {
+                // The input is an string with all whitespace characters.
+                return 0;
+            }
+
+            int result = 0;
+            char currentChar;
+
+            int maxDigitCode = (int)'9';
+            int minDigitCode = (int)'0';
+            do
+            {
+                currentChar = input[startIndex++];
+                if (currentChar < minDigitCode || currentChar > maxDigitCode)
+                    break;
+
+                int digitValue = currentChar - minDigitCode;
+                result = result * 10 + digitValue;
+
+
+            } while (startIndex < input.Length);
+
+            return result * sign;
+        }
+    }
+}
---
+++ test/AM.Core.Algorithms.Numbers.Tests/NumberUtilitiesTests.cs
@@ -0,0 +1,77 @@
+using System;
+using Xunit;
+
+namespace AM.Core.Algorithms.Numbers.Tests
+{
+    public class NumberUtilitiesTests
+    {
+        [Fact]
+        public void Reverse_Succeeds()
+        {
+            Assert.Equal(4321, NumberUtilities.Reverse(1234));
+        }
+
+        [Fact]
+        public void Reverse_SucceedsForNegativeNumbers()
+        {
+            Assert.Equal(-4321, NumberUtilities.Reverse(-1234));
+        }
+
+        [Fact]
+        public void Reverse_Returns0For0()
+        {
+            Assert.Equal(0, NumberUtilities.Reverse(0));
+        }
+
+        [Fact]
+        public void Reverse_ThrowsForTooBigNumber()
+        {
+            const int input = 1534236469;
+            Assert.Equal(0, NumberUtilities.Reverse(input));
+        }
+
+        [Fact]
+        public void ToInt_SucceedsForPositiveNumber()
+        {
+            Assert.Equal(1234, NumberUtilities.ToInt("1234"));
+        }
+
+        [Fact]
+        public void ToInt_SucceedsForPositiveNumberWithSignPrefix()
+        {
+            Assert.Equal(1234, NumberUtilities.ToInt("+1234"));
+        }
+
+        [Fact]
+        public void ToInt_SucceedsForNegativeNumber()
+        {
+            Assert.Equal(-1234, NumberUtilities.ToInt("-1234"));
+        }
+
+        [Fact]
+        public void ToInt_SucceedsForPositiveNumberWithEmptyPrefix()
+        {
+            Assert.Equal(1234, NumberUtilities.ToInt("      1234"));
+        }
+
+        [Fact]
+        public void ToInt_SucceedsForNegativeNumberWithEmptyPrefix()
+        {
+            Assert.Equal(-1234, NumberUtilities.ToInt("      -1234"));
+        }
+
+        [Theory]
+        [InlineData("0")]
+        [InlineData("+0")]
+        [InlineData("   +0    ")]
+        [InlineData("   +0asdfasdf")]
+        [InlineData("   +0  ")]
+        [InlineData("   -0  ")]
+        [InlineData("-0  ")]
+        [InlineData("-0")]
+        public void ToInt_SucceedsForZero(string input)
+        {
+            Assert.Equal(0, NumberUtilities.ToInt(input));
+        }
+    }
+}
