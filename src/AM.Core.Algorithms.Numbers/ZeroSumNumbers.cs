e362b44094bec65bc05420eb55683e6d98caf6fe
Artak Mkrtchyan <artak@outlook.com>
2019-09-08 23:18:10 -0700

    Added 3Sum algorithm implementation

---
+++ src/AM.Core.Algorithms.Numbers/ZeroSumNumbers.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AM.Core.Algorithms.Numbers
+{
+    public static class ZeroSumNumbers
+    {
+        /// <summary>
+        /// This is the LeetCode 3Sum problem implementation: https://leetcode.com/problems/3sum/
+        /// </summary>
+        /// <param name="numbers">The input numbers</param>
+        /// <returns>The unique list of tries which sume to zero.</returns>
+        public static IList<IList<int>> GetZeroSumTries(int[] numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            IList<IList<int>> result = new List<IList<int>>();
+
+            numbers = numbers.OrderBy(i => i).ToArray();
+
+            // Filter out the elements from the array, which won't be used in the result in any way
+            // Assume the elements go like A1, A2, ... Amin, Amin+1, ... Amax, Amax+1, ... An
+            // 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int leftIndex = i + 1;
+                int rightIndex = numbers.Length - 1;
+
+                while (leftIndex < rightIndex)
+                {
+                    int sum = numbers[i] + numbers[leftIndex] + numbers[rightIndex];
+                    if (sum == 0)
+                    {
+                        result.Add(new int[] { numbers[i], numbers[leftIndex], numbers[rightIndex] });
+
+                        // Skip over the repeating numbers. We can do this as the list is ordered.
+                        SkipWhile(numbers, ref leftIndex, 1);
+                        SkipWhile(numbers, ref rightIndex, -1);
+
+                        leftIndex++;
+                        rightIndex--;
+                        continue;
+                    }
+                    else if (sum < 0)
+                    {
+                        leftIndex++;
+                    }
+                    else
+                    {
+                        rightIndex--;
+                    }
+                }
+
+                SkipWhile(numbers, ref i, 1);
+            }
+
+            return result;
+        }
+
+        private static void SkipWhile(int[] array, ref int index, int moveDirection)
+        {
+            int nextIndex = index;
+            while (nextIndex >= 0 && nextIndex < array.Length && array[index] == array[nextIndex])
+            {
+                index = nextIndex;
+                nextIndex += moveDirection;
+            }
+        }
+    }
+}
---
+++ test/AM.Core.Algorithms.Numbers.Tests/ZeroSumNumbersTests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace AM.Core.Algorithms.Numbers.Tests
+{
+    public class ZeroSumNumbersTests
+    {
+        [Fact]
+        public void Sum_ForZero()
+        {
+            int[] input = new int[] { 0, 0, 0, 0 };
+            var result = ZeroSumNumbers.GetZeroSumTries(input);
+
+            Assert.Equal(1, result.Count);
+
+            Assert.All(result[0], i => Assert.Equal(0, i));
+        }
+
+        [Fact]
+        public void Sum_Succeeds()
+        {
+            int[] input = new int[] { -4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0 };
+
+            var result = ZeroSumNumbers.GetZeroSumTries(input);
+
+            Assert.Equal(6, result.Count);
+            Assert.Contains(new[] { -5, 1, 4 }, result);
+            Assert.Contains(new[] { -4, 0, 4 }, result);
+            Assert.Contains(new[] { -4, 1, 3 }, result);
+            Assert.Contains(new[] { -2, -2, 4 }, result);
+            Assert.Contains(new[] { -2, 1, 1 }, result);
+            Assert.Contains(new[] { 0, 0, 0 }, result);
+        }
+    }
+}
