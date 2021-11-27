fb2232cc72cd6d9d7ba5a18bfc196598ea974d72
Artak Mkrtchyan <artak.services+github@outlook.com>
2021-11-27 12:52:33 -0800

    Added valid parenthesis generation problem

---
+++ src/AM.Core.Algorithms.Combinatorics/ValidParenthesis.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AM.Core.Algorithms.Combinatorics;
+
+public class ValidParenthesis
+{
+    public static IList<string> GenerateParenthesis(int n)
+    {
+        IList<string> result = new List<string>();
+        GetValidPermuations(result, string.Empty, n, n);
+
+        return result;
+    }
+
+    private static void GetValidPermuations(IList<string> result, string match, int openingBracesToAdd, int closingBracesToAdd)
+    {
+        if (openingBracesToAdd == 0 && closingBracesToAdd == 0)
+            result.Add(match);
+
+        if (openingBracesToAdd > 0)
+            GetValidPermuations(result, match + "(", openingBracesToAdd - 1, closingBracesToAdd);
+
+        if (closingBracesToAdd > openingBracesToAdd)
+            GetValidPermuations(result, match + ")", openingBracesToAdd, closingBracesToAdd - 1);
+    }
+}
---
+++ test/AM.Core.Algorithms.Combinatorics.Tests/ValidParenthesisTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace AM.Core.Algorithms.Combinatorics.Tests
+{
+    public class ValidParenthesisTests
+    {
+        [Fact]
+        public void GetValidPermuations_SucceedsForSingleSet()
+        {
+            var result = ValidParenthesis.GenerateParenthesis(1);
+
+            Assert.Equal(1, result.Count);
+
+            Assert.Equal("()", result[0]);
+        }
+
+        [Fact]
+        public void GetValidPermuations_SucceedsForDoubleSet()
+        {
+            var result = ValidParenthesis.GenerateParenthesis(2);
+
+            Assert.Equal(2, result.Count);
+
+            Assert.Contains("()()", result);
+            Assert.Contains("(())", result);
+        }
+    }
+}
