1525c1cb38dacfacf5a660c8a5c8271ed6005db9
Artak Mkrtchyan <artak.services+github@outlook.com>
2022-02-26 12:46:20 -0800

    Refactoring

--- src/AM.Core.Algorithms.Puzzles/SudokuSolver.cs
+++ src/AM.Core.Algorithms.Puzzles/SudokuSolver.cs
@@ -6,6 +6,8 @@ namespace AM.Core.Algorithms.Puzzles
 {
     public class SudokuSolver
     {
+        private static char[] globalOptions = new[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
         public void SolveSudoku(char[][] board)
         {
             // Iterate over each empty cell and try to find such that can have only one value
@@ -18,119 +20,95 @@ namespace AM.Core.Algorithms.Puzzles
 
         private static bool Solve(char[][] board)
         {
-            bool boardChanged = true;
-            List<int[]> emptyCells = GetAllEmptyCells(board);
-            Debug.WriteLine($"Trying to solve a board with {emptyCells.Count} empty cells.");
-            PrintGrid(board);
+            Stack<char[][]> baordStates = new Stack<char[][]>();
 
+            if (!TryPopulateEmptyCells(board, out var emptyCells))
+                return false;
+
+            if (emptyCells.Count == 0)
+            {
+                // There are no more empty cells left. The board was filled
+                return true;
+            }
+
+            // Store the snapshot of current board in case the next option doens't work out
+            var snapshot = GetTempBoard(board);
+
+            // There are still cells that we couldn't find a good answer for. Try out options now.
+            for (var i = 0; i < emptyCells.Count; i++)
+            {
+                var cell = emptyCells[i];
+
+                var options = FindOptionsForCell(board, cell.Row, cell.Col);
+
+                foreach (var option in options)
+                {
+                    board[cell.Row][cell.Col] = option;
+                    if (Solve(board))
+                        return true;
+
+                    // Restore back to the original state
+                    CopyBoard(snapshot, board);
+                }
+            }
+
+            return false;
+        }
+
+        private static bool TryPopulateEmptyCells(char[][] board, out List<Cell> emptyCells)
+        {
+            emptyCells = GetAllEmptyCells(board);
+            bool boardChanged = true;
             while (emptyCells.Count > 0 && boardChanged)
             {
                 boardChanged = false;
 
-                Debug.WriteLine($"Trying to fill {emptyCells.Count} empty cells");
-
                 for (int i = 0; i < emptyCells.Count; i++)
                 {
-                    int[] cell = emptyCells[i];
-                    int row = cell[0];
-                    int col = cell[1];
-                    char[] options = FindOptionsForCell(board, row, col);
+                    var cell = emptyCells[i];
+                    char[] options = FindOptionsForCell(board, cell.Row, cell.Col);
                     if (options.Length == 0)
                     {
-                        Debug.WriteLine($"No options available for cell {row}:{col}. DEADLOCK!!!");
                         return false;
                     }
 
                     if (options.Length == 1)
                     {
-                        Debug.WriteLine($"Found a match for position {row}:{col} value {options[0]}");
-
-                        board[row][col] = options[0];
+                        board[cell.Row][cell.Col] = options[0];
                         emptyCells.RemoveAt(i);
                         if (!boardChanged)
                             boardChanged = true;
                     }
-                    else
-                    {
-                        Debug.WriteLine($"Multiple options are available for position {row}:{col}. {String.Join(",", options)}");
-                    }
                 }
             }
 
-            PrintGrid(board);
-            if (emptyCells.Count > 0)
-            {
-                // There are still cells that we couldn't find a good answer for. Try out options now.
-                Debug.WriteLine($"Processing cells with multiple options");
-                for (var i = 0; i < emptyCells.Count; i++)
-                {
-                    var cell = emptyCells[i];
-                    var options = FindOptionsForCell(board, cell[0], cell[1]);
-                    foreach (var option in options)
-                    {
-                        var row = cell[0];
-                        var col = cell[1];
-
-                        var copyBoard = GetTempBoard(board);
-                        copyBoard[row][col] = option;
-                        Debug.WriteLine($"Trying out value {option} for cell {row}:{col}");
-                        if (Solve(copyBoard))
-                        {
-                            CopyBoard(copyBoard, board);
-                            board = copyBoard;
-                            Debug.WriteLine($"Success for value {option} for cell {row}:{col}");
-                            return true;
-                        }
-                    }
-                }
-
-                Debug.WriteLine("Found no solution for suspicious cells");
-                return false;
-            }
-            else
-            {
-                Debug.WriteLine("Success");
-                return true;
-            }
+            return true;
         }
 
-        private static void CopyBoard(char[][] source, char[][] dest)
+        private static List<Cell> GetAllEmptyCells(char[][] board)
         {
-            for (int i = 0; i < source.Length; i++)
-            {
-                for (int j = 0; j < source[i].Length; j++)
-                {
-                    dest[i][j] = source[i][j];
-                }
-            }
-        }
-
-        private static List<int[]> GetAllEmptyCells(char[][] board)
-        {
-            List<int[]> result = new List<int[]>();
+            var result = new List<Cell>();
 
             for (var i = 0; i < board.Length; i++)
             {
                 for (var j = 0; j < board[i].Length; j++)
                 {
-                    if (IsEmpty(board[i][j]))
-                        result.Add(new[] { i, j });
+                    if (IsCellEmpty(board[i][j]))
+                        result.Add(new Cell(i, j));
                 }
             }
 
             return result;
         }
 
-        private static bool IsEmpty(char value) => value == '.';
+        private static bool IsCellEmpty(char cellValue) => cellValue == '.';
 
         private static char[] FindOptionsForCell(char[][] board, int row, int col)
         {
-            var globalOptions = new[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-
             List<char> result = new List<char>();
             foreach (var option in globalOptions)
             {
-                if (IsOptionAllowedInRow(board, row, option) && IsOptionAllowedInCol(board, col, option) && IsOptionAllowedInBox(board, row, col, option))
+                if (IsValueAllowedInRow(board, row, option) && IsValueAllowedInColumn(board, col, option) && IsValueAllowedInBox(board, row, col, option))
                 {
                     result.Add(option);
                 }
@@ -139,7 +117,7 @@ namespace AM.Core.Algorithms.Puzzles
             return result.ToArray();
         }
 
-        private static bool IsOptionAllowedInRow(char[][] grid, int row, char option)
+        private static bool IsValueAllowedInRow(char[][] grid, int row, char option)
         {
             for (var col = 0; col < grid[row].Length; col++)
             {
@@ -150,7 +128,7 @@ namespace AM.Core.Algorithms.Puzzles
             return true;
         }
 
-        private static bool IsOptionAllowedInCol(char[][] grid, int col, char option)
+        private static bool IsValueAllowedInColumn(char[][] grid, int col, char option)
         {
             for (var row = 0; row < grid.Length; row++)
             {
@@ -161,7 +139,7 @@ namespace AM.Core.Algorithms.Puzzles
             return true;
         }
 
-        private static bool IsOptionAllowedInBox(char[][] grid, int row, int col, char option)
+        private static bool IsValueAllowedInBox(char[][] grid, int row, int col, char option)
         {
             var boxRowStart = GetSubBoxStart(row);
             var boxColStart = GetSubBoxStart(col);
@@ -180,6 +158,7 @@ namespace AM.Core.Algorithms.Puzzles
         private static char[][] GetTempBoard(char[][] board)
         {
             char[][] result = new char[board.Length][];
+
             for (int i = 0; i < board.Length; i++)
             {
                 result[i] = new char[board[i].Length];
@@ -190,12 +169,28 @@ namespace AM.Core.Algorithms.Puzzles
             return result;
         }
 
-        private static void PrintGrid(char[][] grid)
+        private static void CopyBoard(char[][] source, char[][] dest)
         {
-            for (int i = 0; i < grid.Length; i++)
+            for (int i = 0; i < source.Length; i++)
             {
-                Debug.WriteLine(string.Join(", ", grid[i]));
+                for (int j = 0; j < source[i].Length; j++)
+                {
+                    dest[i][j] = source[i][j];
+                }
             }
+        }
+
+        private readonly struct Cell
+        {
+            public Cell(int row, int col)
+            {
+                Row = row;
+                Col = col;
+            }
+
+            public int Row { get; }
+
+            public int Col { get; }
         }
     }
 }
