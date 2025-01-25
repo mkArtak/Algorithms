2dc872af5d730bcd9e7efc9b23692a10a6a7ad2b
mkArtak <artak.services+github@outlook.com>
2025-01-25 13:36:11 -0800

    Added the binary tree restore problem

---
+++ src/AM.Core.Algorithms.Trees/BinaryTreeUtilities.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.Core.Algorithms.Trees;
+
+public static class BinaryTreeUtilities
+{
+    /// <summary>
+    /// Given two integer arrays preorder and inorder, where preorder is the preorder traversal
+    /// of a binary tree and inorder is the inorder traversal of the same tree,
+    /// construct and return the binary tree.
+    /// </summary>
+    /// <param name="inorderTraversal"></param>
+    /// <param name="preorderTraversal"></param>
+    /// <returns>The root of the constructed tree</returns>
+    public static BinaryTreeNode<int> RestoreFromArrays(int[] preorderTraversal, int[] inorderTraversal)
+    {
+        return RestoreFromArrays(preorderTraversal.AsSpan(), inorderTraversal.AsSpan());
+    }
+
+    private static BinaryTreeNode<int> RestoreFromArrays(Span<int> preorder, Span<int> inorder)
+    {
+        var rootValue = preorder[0];
+        BinaryTreeNode<int> root = new BinaryTreeNode<int>(rootValue);
+        var separatorIndex = inorder.IndexOf(rootValue);
+        var leftSubtreeElementCount = separatorIndex;
+        if (leftSubtreeElementCount > 0)
+            root.Left = RestoreFromArrays(preorder[1..(1 + separatorIndex)], inorder[0..separatorIndex]);
+
+        var rightSubtreeElementCount = inorder.Length - separatorIndex - 1;
+        if (rightSubtreeElementCount > 0)
+            root.Right = RestoreFromArrays(preorder[^rightSubtreeElementCount..], inorder[^rightSubtreeElementCount..]);
+
+        return root;
+    }
+}
---
+++ test/AM.Core.Algorithms.Trees.Tests/BinaryTreeUtilitiesTests.cs
@@ -0,0 +1,33 @@
+﻿namespace AM.Core.Algorithms.Trees.Tests;
+
+[TestClass]
+public class BinaryTreeUtilitiesTests
+{
+    [TestMethod]
+    public void RestoreFromArrays_Succeeds()
+    {
+        // Setup
+        int[] preorder = [3, 9, 20, 15, 7];
+        int[] inorder = [9, 3, 15, 20, 7];
+
+        // Act
+        BinaryTreeNode<int> result = BinaryTreeUtilities.RestoreFromArrays(preorder, inorder);
+
+        // Validate
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(3, result.Value);
+
+        Assert.IsNotNull(result.Left);
+        Assert.AreEqual(9, result.Left.Value);
+
+        Assert.IsNotNull(result.Right);
+        Assert.AreEqual(20, result.Right.Value);
+
+        Assert.IsNotNull(result.Right.Left);
+        Assert.AreEqual(15, result.Right.Left.Value);
+
+        Assert.IsNotNull(result.Right.Right);
+        Assert.AreEqual(7, result.Right.Right.Value);
+    }
+}
